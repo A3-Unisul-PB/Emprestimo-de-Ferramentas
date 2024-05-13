57ee0a1a3d68a62c47938c0c1cb921795d3edeb3
1072410915 <1072410915@UNS11DT099709.animaedu.intranet>
2024-05-13 19:16:38 -0300

    feat: template inicial

---
+++ A3/src/main/java/model/Titiu.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author 1072410915
+ */
+public class Titiu {
+    
+    //teste
+}
---
+++ A3/src/main/java/principal/Principal.java
@@ -0,0 +1,11 @@
+
+
+package principal;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
