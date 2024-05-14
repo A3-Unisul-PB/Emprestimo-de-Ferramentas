1c9a0d32abbbd469b492b18ca846dc751755b438
Pedro Nieto <pedrnieto.2005@gmail.com>
2024-05-14 19:02:57 -0300

    refactor(principal): Realocate dao package
    Since the project was inside a unecessary folder, the package was realocated

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,15 @@
+
+
+package principal;
+
+import view.FrmMenuPrincipal;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmMenuPrincipal menu = new FrmMenuPrincipal();
+        menu.setVisible(true);
+        
+    }
+}
