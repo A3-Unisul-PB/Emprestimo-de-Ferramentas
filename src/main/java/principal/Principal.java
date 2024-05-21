82db4f36475fa1ee42398b77a24b51a4a1a6bd2e
heito <heito@PC>
2024-05-21 11:08:56 -0300

    docs: documentação da classe Principal

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,10 +1,27 @@
+/**
+ * O pacote principal contém a classe principal do programa.
+ */
 package principal;
 
+// Importação da classe FrmMenuPrincipal do pacote view.
 import view.FrmMenuPrincipal;
 
+/**
+ * A classe Principal é a classe principal do programa.
+ * Ela contém o método main, que é o ponto de entrada do programa.
+ */
 public class Principal {
+    /**
+     * O método main é o ponto de entrada do programa.
+     * Ele cria uma instância da classe FrmMenuPrincipal e a torna visível.
+     *
+     * @param args Argumentos da linha de comando. Não são utilizados neste programa.
+     */
     public static void main(String[] args) {
+        // Cria uma nova instância da classe FrmMenuPrincipal.
         FrmMenuPrincipal menu = new FrmMenuPrincipal();
+        
+        // Torna a instância do menu visível.
         menu.setVisible(true);
     }
 }
