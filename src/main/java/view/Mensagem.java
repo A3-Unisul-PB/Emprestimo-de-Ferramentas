c0bf14eb324b6de8a89204746c5266d327211a91
heito <heito@PC>
2024-05-21 11:21:51 -0300

    docs: documentação do view Mensagem

--- src/main/java/view/Mensagem.java
+++ src/main/java/view/Mensagem.java
@@ -1,7 +1,19 @@
+/**
+ * O pacote view contém a classe Mensagem.
+ */
 package view;
 
+/**
+ * A classe Mensagem é uma subclasse da classe Exception.
+ * Ela é usada para criar uma exceção personalizada com uma mensagem específica.
+ */
 public class Mensagem extends Exception {
 
+    /**
+     * Construtor que inicializa a exceção Mensagem com uma mensagem específica.
+     *
+     * @param mensagem A mensagem da exceção.
+     */
     public Mensagem(String mensagem){
         super(mensagem);
     }
