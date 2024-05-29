ded9dfe8c8949e9c8f2b9ac7ddefae282add7683
Vinicius Freitas da Silva <vinicius.scp999@gmail.com>
2024-05-28 21:25:36 -0300

    feat: adicionado metodo getAtrasados

--- src/main/java/dao/RelatorioDAO.java
+++ src/main/java/dao/RelatorioDAO.java
@@ -36,5 +36,28 @@ public class RelatorioDAO {
         return minhaLista;
     }
     
-    
+        public ArrayList<Relatorio> getAtrasados(){
+        minhaLista.clear();
+        
+        String query = "SELECT a.nome AS nome_amigo, \n" +
+            "f.nome AS nome_ferramenta " +
+            "FROM tb_emprestimos e " +
+            "JOIN tb_amigos a ON e.id_amigo = a.id " +
+            "JOIN tb_ferramentas f ON e.id_ferramenta = f.id " +
+            "WHERE e.data_limite < NOW();";
+        
+        try {
+            Statement stmt = db.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery(query);
+            
+            while(res.next()){
+                String amigoNome = res.getString("nome_amigo");
+                String ferramentaNome = res.getString("nome_ferramenta");
+            }
+            stmt.close();
+        } catch (SQLException ex){
+            System.out.println("Erro: "+ex);
+        }
+        return minhaLista;
+    }
 }
