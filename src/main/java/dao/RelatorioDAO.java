32261a27094d51f8af2f7e49b6a6c4cdf76b69da
Vinicius Freitas da Silva <vinicius.scp999@gmail.com>
2024-05-28 21:00:56 -0300

    feat: RelatorioDAO.java classe criado e adicionado metodo getTotais

---
+++ src/main/java/dao/RelatorioDAO.java
@@ -0,0 +1,40 @@
+package dao;
+
+import model.Relatorio;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class RelatorioDAO {
+    
+    private ArrayList<Relatorio> minhaLista = new ArrayList<>();
+    private DatabaseConnection db;
+
+    public RelatorioDAO() {
+        this.db = new DatabaseConnection();
+    }
+    
+    public ArrayList<Relatorio> getTotais(){
+        minhaLista.clear();
+        
+        String query = "SELECT a.nome AS nome_amigo, f.nome AS nome_ferramenta "+
+                "FROM tb_emprestimos e "+
+                "JOIN tb_amigos a ON e.id_amigo = a.id "+
+                "JOIN tb_ferramentas f ON e.id_ferramenta = f.id;";
+        
+        try {
+            Statement stmt = db.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery(query);
+            
+            while(res.next()){
+                String amigoNome = res.getString("nome_amigo");
+                String ferramentaNome = res.getString("nome_ferramenta");
+            }
+            stmt.close();
+        } catch (SQLException ex){
+            System.out.println("Erro: "+ex);
+        }
+        return minhaLista;
+    }
+    
+    
+}
