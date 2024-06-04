49ce057eb7711663971c2216ac912bfbd3183856
Pedro Nieto <pedronieto.2005@gmail.com>
2024-06-04 19:48:18 -0300

    feat: Add SELECT queries for "Relatório"
    Added 3 SQL queries for "Relatório" loans.

--- banco.sql
+++ banco.sql
@@ -1,3 +1,4 @@
+
 --Amigos--
 CREATE TABLE tb_amigos (
 	id INT NOT NULL PRIMARY KEY,
@@ -37,13 +38,34 @@ CREATE TABLE tb_emprestimos (
 	FOREIGN KEY (id_ferramenta) REFERENCES tb_ferramentas (id)
 );
 
-
+--Empréstimo geral--
 INSERT INTO tb_emprestimos (id, id_amigo, id_ferramenta) VALUES
 	(1, 1, 1),
 	(2, 2, 3),
 	(3, 3, 2);
 
-SELECT a.nome AS nome_amigo, f.nome AS nome_ferramenta
+--Empréstimo pendente--
+INSERT INTO tb_emprestimos (id, id_amigo, id_ferramenta, data_limite) VALUES (4, 4, 2, '2024-06-03');
+
+--Empréstimo concluído--
+INSERT INTO tb_emprestimos (id, id_amigo, id_ferramenta, data_finalizado) VALUES (5, 5, 3, '2024-06-03');
+
+--Empréstimos totais--
+SELECT a.nome AS nome_amigo, a.telefone, f.nome AS nome_ferramenta, f.preco
 	FROM tb_emprestimos e
 	JOIN tb_amigos a ON e.id_amigo = a.id
 	JOIN tb_ferramentas f ON e.id_ferramenta = f.id;
+
+--Empréstimos pendentes--
+SELECT a.nome AS nome_amigo, a.telefone, f.nome AS nome_ferramenta, f.preco
+	FROM tb_emprestimos e
+    JOIN tb_amigos a ON e.id_amigo = a.id
+    JOIN tb_ferramentas f ON e.id_ferramenta = f.id
+    WHERE e.data_limite IS NOT NULL AND e.data_limite < CURRENT_DATE AND e.data_finalizado IS NULL;
+
+--Empréstimos concluídos--
+SELECT a.nome AS nome_amigo, a.telefone, f.nome AS nome_ferramenta, f.preco
+	FROM tb_emprestimos e
+    JOIN tb_amigos a ON e.id_amigo = a.id
+    JOIN tb_ferramentas f ON e.id_ferramenta = f.id
+    WHERE e.data_finalizado IS NOT NULL;
