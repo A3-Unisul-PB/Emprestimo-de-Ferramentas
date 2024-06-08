08dcc12ed2fad5cc1fb62c755dcd3251c16df268
Pedro Nieto <pedronieto.2005@gmail.com>
2024-06-08 15:08:43 -0300

    wip: Fixing Amigo and AmigoDAO

--- src/main/java/dao/AmigoDAO.java
+++ src/main/java/dao/AmigoDAO.java
@@ -7,27 +7,57 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import model.Amigo;
 
+/**
+ * Classe responsável pelo acesso aos dados dos amigos no banco de dados.
+ * Fornece dados para carregar, inserir, atualizar e excluir amigos.
+ *
+ * @author joaopedrofariasdasilva
+ */
 public class AmigoDAO {
 
+    /**
+     * Lista de amigos carregados do banco de dados.
+     */
     public ArrayList<Amigo> minhaLista = new ArrayList<>();
+
+    /**
+     * Objeto responsável pela conexão com o banco de dados.
+     */
     private DatabaseConnection db;
 
+    /**
+     * Construtor da classe AmigoDAO.
+     *
+     * Inicializa a conexão com o banco de dados.
+     */
     public AmigoDAO() {
         this.db = new DatabaseConnection();
     }
 
+    /**
+     * Retorna a lista de amigos do banco de dados.
+     *
+     * @return - ArrayList contendo objetos Amigo recuperados do banco de dados.
+     *
+     */
     public ArrayList<Amigo> getMinhaLista() {
+
+        /**
+         * Limpa nosso ArrayList.
+         */
         minhaLista.clear();
 
         try {
             Statement stmt = db.getConexao().createStatement();
             ResultSet res = stmt.executeQuery("SELECT * FROM tb_amigos");
             while (res.next()) {
-                int id = res.getInt("id");
+
+                int id = res.getInt("id_amigo");
                 String nome = res.getString("nome");
                 String telefone = res.getString("telefone");
 
                 Amigo objeto = new Amigo(id, nome, telefone);
+
                 minhaLista.add(objeto);
             }
             stmt.close();
@@ -38,15 +68,26 @@ public class AmigoDAO {
         return minhaLista;
     }
 
+    /**
+     * Define a lista de amigos. Substitui a lista atual de amigos por uma nova
+     * lista fornecida como parâmetro.
+     *
+     * @param minhaLista
+     */
     public void setMinhaLista(ArrayList<Amigo> minhaLista) {
         this.minhaLista = minhaLista;
     }
 
+    /**
+     * Retorna o maior ID presente na tabela de amigos.
+     *
+     * @return - Inteiro representando o maior ID.
+     */
     public int maiorID() {
         int maiorID = 0;
         try {
             Statement stmt = db.getConexao().createStatement();
-            ResultSet res = stmt.executeQuery("SELECT MAX(id) id FROM tb_amigos");
+            ResultSet res = stmt.executeQuery("SELECT MAX(id_amigo) id FROM tb_amigos");
             res.next();
             maiorID = res.getInt("id");
             stmt.close();
@@ -56,8 +97,14 @@ public class AmigoDAO {
         return maiorID;
     }
 
+    /**
+     * Cadastra novo amigo no banco de dados.
+     *
+     * @param objeto - Objeto Amigo a ser inserido.
+     * @return - Boolean indicando se a operação foi bem-sucedida.
+     */
     public boolean insertAmigoBD(Amigo objeto) {
-        String sql = "INSERT INTO tb_amigos(id,nome,telefone) VALUES(?,?,?)";
+        String sql = "INSERT INTO tb_amigos(id_amigo,nome,telefone) VALUES(?,?,?)";
         try {
             PreparedStatement stmt = db.getConexao().prepareStatement(sql);
 
@@ -75,10 +122,16 @@ public class AmigoDAO {
         }
     }
 
+    /**
+     * Exclui um amigo do banco de dados com base no ID.
+     *
+     * @param id - inteiro representando o ID do amigo a ser excluído.
+     * @return - Boolean indicando se a operação foi bem-sucedida.
+     */
     public boolean deleteAmigoBD(int id) {
         try {
             Statement stmt = db.getConexao().createStatement();
-            stmt.executeUpdate("DELETE FROM tb_amigos WHERE id = " + id);
+            stmt.executeUpdate("DELETE FROM tb_amigos WHERE id_amigo = " + id);
             stmt.close();
 
         } catch (SQLException erro) {
@@ -87,8 +140,15 @@ public class AmigoDAO {
         return true;
     }
 
+    /**
+     * Atualiza os dados de um amigo no banco de dados.
+     *
+     * @param objeto - Objeto Amigo com os dados atualizados.
+     * @return - Boolean indicando se a operação foi bem-sucedida.
+     */
     public boolean updateAmigoBD(Amigo objeto) {
-        String sql = "UPDATE tb_amigos set nome = ?, telefone = ? WHERE id = ?";
+
+        String sql = "UPDATE tb_amigos set nome = ? ,telefone = ? WHERE id_amigo = ?";
 
         try {
             PreparedStatement stmt = db.getConexao().prepareStatement(sql);
@@ -108,12 +168,19 @@ public class AmigoDAO {
         }
     }
 
+    /**
+     * Carrega um amigo do banco de dados com base no ID.
+     *
+     * @param id - Representa o ID do amigo a ser carregado.
+     * @return - Objeto Amigo com os dados do amigo carregado.
+     */
     public Amigo carregaAmigo(int id) {
         Amigo objeto = new Amigo();
         objeto.setId(id);
         try {
             Statement stmt = db.getConexao().createStatement();
-            ResultSet res = stmt.executeQuery("SELECT * FROM tb_amigos WHERE id = " + id);
+
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_amigos WHERE id_amigo = " + id);
             res.next();
 
             objeto.setNome(res.getString("nome"));
@@ -125,6 +192,7 @@ public class AmigoDAO {
         }
         return objeto;
     }
+
     public static int getIdPeloNome(String nome) {
         int id = -1;
 
@@ -132,7 +200,7 @@ public class AmigoDAO {
             /**
              * Prepara uma consulta SQL para obter o ID do amigo pelo nome
              */
-            String query = "SELECT id FROM tb_amigos WHERE nome = ?";
+            String query = "SELECT id_amigo FROM tb_amigos WHERE nome = ?";
             PreparedStatement statement = DatabaseConnection.getConexao().prepareStatement(query);
             statement.setString(1, nome);
             ResultSet resultSet = statement.executeQuery();
@@ -141,7 +209,7 @@ public class AmigoDAO {
              * Se houver um resultado na consulta, obtém o ID
              */
             if (resultSet.next()) {
-                id = resultSet.getInt("id");
+                id = resultSet.getInt("id_amigo");
             }
         } catch (SQLException ex) {
             /**
@@ -156,6 +224,12 @@ public class AmigoDAO {
         return id;
     }
 
+    /**
+     * Método para verificar se um amigo possui empréstimos pendentes
+     *
+     * @param id
+     * @return
+     */
     public boolean verificarPendencia(int id) {
 
         try {
@@ -166,13 +240,13 @@ public class AmigoDAO {
             /**
              * Executa a consulta SQL para obter os empréstimos do amigo
              */
-            ResultSet res = stmt.executeQuery("select id, entregue from tb_emprestimos;");
+            ResultSet res = stmt.executeQuery("select id_amigo, entregue from tb_emprestimos;");
             while (res.next()) {
 
                 /**
                  * Obtém os dados de cada empréstimo
                  */
-                int idAmg = res.getInt("id");
+                int idAmg = res.getInt("id_amigo");
                 boolean entregue = res.getBoolean("entregue");
 
                 /**
